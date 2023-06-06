52d1e90ec496d3f74f78500c36ae5ea74693387c
RhodAyo <aroluwole@gmail.com>
2023-06-06 19:59:25 +0000

    new branch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="text-3xl font-bold underline">
-     <h1>This is a Teeket</h1>
+      <h1>This is a Teeket</h1>
+      <h2>Ayomide wrote this</h2>
     </div>
   );
 }
