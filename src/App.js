e67990b8c8f13fabe8e6c57e8a07dc0a1c2b9d3a
latifat99 <suleimanlatifat0@gmail.com>
2023-06-09 14:26:14 -0700

    landing page for teeket app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from "react";
 import Navbar from "./components/navbar_components/navbar"
+<<<<<<< Updated upstream
 import HomePage from "./pages/home.page";
 
 
+=======
+import Land from "./pages/landing";
+>>>>>>> Stashed changes
 function App() {
+
+ 
   return (
+<<<<<<< Updated upstream
     <div className="App">
      {/* <Navbar /> */}
      <HomePage />
     </div>
+=======
+    <>
+    <Land></Land>
+    </>
+>>>>>>> Stashed changes
   );
 }
 
