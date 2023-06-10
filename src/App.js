8d6202ad9910356c5d68c0326ed21a289654a147
gogurlbaby <philiahammmond@gmail.com>
2023-06-10 00:32:06 +0000

    created path for landing page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Navbar from "./components/navbar_components/navbar"
 import HomePage from "./pages/home.page";
 import Login from "./pages/login";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Land from "./pages/landing";
+import Land from "./pages/landing.page";
 
 function App() {
 
