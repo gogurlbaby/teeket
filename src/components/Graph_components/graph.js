aedc831a72e9091983dca73e7e47ffa1dd8e5e99
latifat99 <suleimanlatifat0@gmail.com>
2023-06-13 09:06:59 -0700

    graph-page

--- src/components/Graph_components/graph.js
+++ src/components/Graph_components/graph.js
@@ -7,10 +7,12 @@ import tee from "../../assets/other-images/general_teeket_logo.svg"
  import btn from "../../assets/graph_images/dropdown - btn.png"
  import drop from"../../assets/graph_images/dropdown-list (1).png"
 import info from "../../assets/graph_images/Info.jpg"
-import jane from "../../assets/graph_images/jane.jpg"
-import john from "../../assets/graph_images/john.jpg"
-import daniel from "../../assets/graph_images/daniel.jpg"
+import jane2 from "../../assets/graph_images/jane2.jpg"
+import john2 from "../../assets/graph_images/john2.jpg"
+import daniel2 from "../../assets/graph_images/daniel2.jpg"
 import Dropdown from "react-bootstrap/Dropdown"
+import vector from "../../assets/graph_images/Vector.svg"
+
  
 
  function Graph(){
@@ -19,17 +21,20 @@ import Dropdown from "react-bootstrap/Dropdown"
 
     let Tops=[
         {
-         name:john,
+         name:john2,
+         Email : "john.doe@gmail.com",
          Attendees: "12 attendees  " 
 
         },
         {
-            name:jane,
+            name:jane2,
+            Email :"jane.doe@gmail.com",
             Attendees: "81 attendees  " 
    
            },
            {
-            name:daniel,
+            name:daniel2,
+            Email :"daniel_prince2023@go...",
             Attendees: "122 attendees  " 
    
            }
@@ -37,24 +42,28 @@ import Dropdown from "react-bootstrap/Dropdown"
 
          return(
             <>
-            <nav className="flex justify-between p-3 border outline-1 ">
-                  <div>
+            <div  >
+            <nav className="flex  p-3 justify-between border outline-1 ">
+                
+                  <div >
                     <img src={tee} alt="logo"></img>
                   </div>
                   <div>
                     <h3 className="p-2 bg-black text-white rounded-md "> Menu</h3>
                   </div>
+                  
             </nav>
-            <div className="p-2 pt-8   ">
-               <img src={go} className="text-xs font-thin  "  alt="frame"></img>
+            <div className=" flex p-4    ">
+               <img src={vector}   alt="frame"></img>
+               <h3 className="pl-4 pt-1 text-base font-semi-bold">Go Back</h3>
             </div>
-            <div className="flex justify-between p-3" >
-                <div>
+            <div className="flex justify-between p-2" >
+                <div className="p-2">
                     <h2 className="font-semi-bold text-lg pb-2">Registration</h2>
                     <p className="font-light  text-gray-500">see all registered attendees detail</p>
                 </div>
                 <div /*relative pt-2*/>
-                <Dropdown>
+               <Dropdown>
                   <Dropdown.Toggle variant="light" id="dropdown-basic">
                   Past month
                  </Dropdown.Toggle>
@@ -70,12 +79,12 @@ import Dropdown from "react-bootstrap/Dropdown"
             </div>
 
             <div className="flex justify-center p-2">
-                <img src={graph1} className="w-full p-4" alt="graph"></img>
+                <img src={graph1} className="w-full p-2" alt="graph"></img>
             </div>
             <div className="p-3">
-                <h3 className="text-lg  font-bold ml-4">Event views</h3>
+                <h3 className="text-lg pt-3 font-bold ml-4">Event views</h3>
             </div>
-            <div className="flex justify-between p-2 pl-2 pr-2">
+            <div className="flex justify-between p-1 pl-2 pr-2">
                 <div className="flex flex-col p-2">
                    <h3 className="pb-3 pl-2 text-lg text-gray-400">Total views</h3>
                    <p className="font-bold text-lg pl-3">240</p>
@@ -90,23 +99,28 @@ import Dropdown from "react-bootstrap/Dropdown"
                 </div>
     
             </div>
-            <div className=" flex  p-3">
-                <h3 className="p-2 text-lg font-bold ">Top Referrers</h3>
-                 <img  src={info} className="mt-4  h-4 w-8"alt="info"></img>
+            <div className=" flex p-3 pt-4">
+                <h3 className=" text-lg font-bold ">Top Referrers</h3>
+                 <img  src={info} className="mt-2 ml-2 h-4 w-8"alt="info"></img>
             </div>
             <div>
                 {
                     Tops.map((top,index) =>{
                           return(
-                            <div className=" flex justify-between justify-center p-3">
-                                <img src={top.name} alt="name"></img>
-                                 <h3 className="text-center text-md font-bold pt-2">{top.Attendees}</h3>
+                            <div className=" flex justify-between justify-center ">
+                                 <div className="flex p-3 ">
+                                  <img src={top.name} className="rounded-full " alt="name"></img>
+                                 <h3 className="pl-2 pt-1 font-semi-bold text-gray-600">{top.Email}</h3>
+                                 </div>
+
+                                 <h3 className="text-center text-md  pt-3">{top.Attendees}</h3>
                                 
                             </div>
                           )
                     }
                     )
                 }
+            </div>
             </div>
             </>
          )
