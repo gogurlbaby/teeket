fa5a4f41f641564bab1a9c3a18242bbf97814f0a
gogurlbaby <philiahammmond@gmail.com>
2023-06-15 12:03:04 +0000

    carousel

--- src/components/home-page_components/Testimonials.js
+++ src/components/home-page_components/Testimonials.js
@@ -7,26 +7,10 @@ import Carousel from "./Carousel/Carousel";
 
 function Testimonials() {
   return (
-    <div className="md:flex md:flex-col md:justify-center md:items-center pt-[1rem] pb-[4rem] ">
-      <h4 className="md:text-[20px] font-bold text-[18px] text-[#000000] ml-[1rem]">
+    <div class="md:flex md:flex-col md:justify-center md:items-center pt-[1rem] pb-[4rem] ">
+      <h4 class="md:text-[20px] font-bold text-[18px] text-[#000000] ml-[1rem]">
         Don’t take our word for it
       </h4>
-      <div className="flex">
-        <img
-          classNameName="d-block w-100"
-          src={first_testimony}
-          alt="firsttestimonial"
-        />
-        <img
-          classNameName="d-block w-100"
-          src={second_testimony}
-          alt="secondtestimonial"
-        />
-      </div>
-      <div className="flex mt-[1.5rem] ml-[35%]">
-        <img src={back_arrow} alt="back-arrow" className="mr-[0.813rem]" />
-        <img src={forward_arrow} alt="forward-arrow" className="ml-[0.813rem]" />
-      </div>
       <Carousel />
     </div>
   );
